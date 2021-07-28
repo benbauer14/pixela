02193cfb5141c720273e45be11a7fa72d3aee1f6
Ben Bauer <ben.bauer1@gmail.com>
2021-07-28 13:42:25 -0500

    gaccount and graph created

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 import requests
 
+pixela_endpoint = "https://pixe.la/v1/users"
+
+pixela_parameters = {
+    "token":"ABIOCIAKSDML15124mk", 
+    "username":"bbauer", 
+    "agreeTermsOfService":"yes", 
+    "notMinor":"yes"
+}
+
+#Create account
+# response = requests.post(url=pixela_endpoint, json=pixela_parameters)
+
+pixela_graph_parameters = {
+    "id": "graph1",
+    "name": "Step Count",
+    "unit": "steps",
+    "type": "int",
+    "color": "sora"
+}
+
+headers = {
+    "X-USER-TOKEN": "ABIOCIAKSDML15124mk", 
+}
+
+#Create graph
+response = requests.post(url="https://pixe.la/v1/users/bbauer/graphs",json=pixela_graph_parameters, headers=headers)
+print(response.text)
