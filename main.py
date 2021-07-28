183bae19557a085a3f86f7c74ee7293fce71f793
Ben Bauer <ben.bauer1@gmail.com>
2021-07-28 14:02:42 -0500

    put working

--- main.py
+++ main.py
@@ -25,5 +25,23 @@ headers = {
 }
 
 #Create graph
-response = requests.post(url="https://pixe.la/v1/users/bbauer/graphs",json=pixela_graph_parameters, headers=headers)
+# response = requests.post(url="https://pixe.la/v1/users/bbauer/graphs",json=pixela_graph_parameters, headers=headers)
+
+#post activity to graph
+
+pixela_activity_post = {
+    "date": "20210728",
+    "quantity": "4",
+}
+
+# response = requests.post("https://pixe.la/v1/users/bbauer/graphs/graph1", json=pixela_activity_post, headers=headers)
+
+
+#update a pixel
+pixela_update = {
+    "quantity": "50",
+}
+
+# response = requests.put(url="https://pixe.la/v1/users/bbauer/graphs/graph1/20210728", json=pixela_update, headers=headers)
+
 print(response.text)
